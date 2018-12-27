64c75d4bccf39c44d915e5bf5220e87afa1af5dc
Heck <jakubpovondra@gmail.com>
2018-12-27 02:59:03 +0100

    Pridani if do for.js

--- data/for.js
+++ data/for.js
@@ -2,6 +2,14 @@ var readline = require('readline-sync');
 var znak = readline.question("Zadejte Znak: ");
 var readline = require('readline-sync');
 var pocet = readline.question("Zadejte Pocet: ");
-for (var _i = 0; _i < (pocet); _i++) {
-    console.log(znak.repeat(pocet));
+
+if (pocet > 20) {
+    console.log("Počet je větší než 20!")
 }
+else {
+    for (var _i = 0; _i < (pocet); _i++) {
+        console.log(znak.repeat(pocet));
+    }
+}
+
+
