30e6e185942cb1ab4796c71699a51ac8a2e3eabb
Peanut <arasidatv@gmail.com>
2018-12-27 01:30:01 +0100

    for

--- data/for.js
+++ data/for.js
@@ -1,5 +1,7 @@
-var znak = "*";
-var pocet = 10;
+var readline = require('readline-sync');
+var znak = readline.question("Zadejte Znak: ");
+var readline = require('readline-sync');
+var pocet = readline.question("Zadejte Počet: ");
 for (var _i = 0; _i < (pocet); _i++) {
     console.log(znak.repeat(pocet));
 }
--- data/for.ts
+++ data/for.ts
@@ -1,5 +1,10 @@
-var znak:string = "*";
-var pocet:number = 10;
+var readline = require('readline-sync');
+var znak:string = readline.question("Zadejte Znak: ");
+var readline = require('readline-sync');
+var pocet:number = readline.question("Zadejte Počet: ");
+
 for (var _i = 0; _i < (pocet); _i++) {
     console.log(znak.repeat(pocet))
 }
+
+
